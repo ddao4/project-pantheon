f5abe05d823e89f6295c6acd874ac9eff9936313
ddao4 <ddao4@asu.edu>
2018-04-27 14:59:01 -0700

    Added log for mongodb connection, removed React components for now

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 mongoose.connect('mongodb://jtemple5:Gt5isboss@ds011785.mlab.com:11785/pantheon_test');
 mongoose.Promise = global.Promise;
 
+mongoose.connection.once('open', function(){
+	console.log('Connected to mongodb :)');
+}).on('error', function(error){
+	console.log("Could not connect to mongo db :(");
+})
+
 // access html frontend/images
 app.use(express.static('public'));
 
@@ -20,7 +26,7 @@ app.use(bodyParser.json());
 // if using provided port for something like heroku, it will use process.env.port
 // otherwise it uses 4000/whatever we set it to for local hosting
 app.listen(process.env.port || 4000, function(){
-	console.log('Succesfully connected :)');
+	console.log('Connected to port :)');
 })
 
 // specify to use routes from api.js in routes folder
